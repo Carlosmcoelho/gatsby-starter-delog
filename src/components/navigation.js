84a8d54ad1177598922bea44b05473eb1cbabcff
carlosmcoelho <carloscoelho@protonmail.com>
2020-04-26 19:17:19 +0200

    new about and projects

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,6 +5,8 @@ import ThemeChanger from "../components/themeChanger"
 export default (props) => (
   <nav className="navigation"> 
     <Link to="/about">About</Link>
+    <Link to="/projects">Projects</Link>
+    <Link to="/notes">Notes</Link>
     <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
